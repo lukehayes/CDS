eedd57c6b68f190b67e2805cf9a186291a5c6a6f
Luke Hayes <lukepurplehayes@gmail.com>
2022-03-04 18:07:30 +0000

    Removed old list code.

--- include/list/list.h
+++ include/list/list.h
@@ -8,39 +8,5 @@ typedef struct List
     Node* next;
 }List;
 
-/**
- * Create a new list.
- *
- * @return List* Pointer to the list.
- */
-struct List* ListCreate();
-
-/**
- * Destroy a list
- *
- * @param List* Pointer to the list.
- */
-void ListDestroy(List* list);
-
-/**
- * Insert a value into the list.
- * 
- * @param List* list
- * @param int value
- */
-void ListInsert(List* list, int value);
-
-
-void ListPrint(List* list)
-{
-    Node* head = list->next;
-
-    while(head->next != NULL)
-    {
-        printf("Value %i \n", head->value);
-        head = head->next;
-    }
-}
-
 
 #endif // CDS_LIST_H
--- src/list.c
+++ src/list.c
@@ -1,26 +1,2 @@
 #include "list/list.h"
 #include <stdlib.h>
-
-List* ListCreate()
-{
-    List* list = malloc(sizeof(List));
-    list->next = null;
-    return list;
-}
-
-void ListDestroy(List* list)
-{
-    free(list);
-}
-
-
-void ListInsert(List* list, int value)
-{
-    Node* head = list->next;
-
-    if(head == NULL)
-    {
-        head->value = value;
-    }
-}
-
