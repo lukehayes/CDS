80835939c410c88e8f38f149dcd34cd22d88fd54
Luke Hayes <lukepurplehayes@gmail.com>
2022-03-21 18:08:22 +0000

    main.c now tests Stack* functions.

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include "list/list.h"
+#include "stack/stack.h"
 
 int main()
 {
-    List* list = ListCreate();
+    Stack s = StackCreate(5, INT);
 
-    ListInsert(list, 100);
+    StackInsert(&s, (int*)100);
+    StackInsert(&s, (int*)200);
+    StackInsert(&s, (int*)500);
 
-    ListDestroy(list);
+    printf("Data: %i \n", ((int*)s.data)[0]);
+
+    /*StackDestroy(&s);*/
+
     return 0 ;
 }
