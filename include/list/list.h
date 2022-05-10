caf1b1bfeecb1ce8d27b9999d61e5a8010eec0aa
Luke Hayes <lukepurplehayes@gmail.com>
2022-05-10 16:50:23 +0100

    Added ListCreate() function.

--- include/list/list.h
+++ include/list/list.h
@@ -2,11 +2,21 @@
 #define CDS_LIST_H
 
 #include "list/node.h"
+#include "DataType.h"
 
 typedef struct List
 {
     Node* head;
 }List;
 
+/**
+ * Create a linked list data structure.
+ *
+ * @param size_t size      The number of elements
+ * @param DataType type    The type of data the stack will store.  
+ *
+ * @return List*.
+ */
+List* ListCreate(size_t size, DataType type);
 
 #endif // CDS_LIST_H
--- src/list.c
+++ src/list.c
@@ -1,2 +1,9 @@
 #include "list/list.h"
 #include <stdlib.h>
+
+List* ListCreate(size_t size, DataType type)
+{
+    List* list = malloc(sizeof(List));
+    list->head = NULL;
+    return list;
+}
